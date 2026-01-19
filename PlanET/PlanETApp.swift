8e8ef4301a6a1f780cb1bff1ffcdfd7e35794098
Leah <lee3267@purdue.edu>
2026-01-19 08:50:52 -0500

    Initial Commit

---
+++ PlanET/PlanETApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanETApp.swift
+//  PlanET
+//
+//  Created by SeungHyeon Lee on 1/19/26.
+//
+
+import SwiftUI
+
+@main
+struct PlanETApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
